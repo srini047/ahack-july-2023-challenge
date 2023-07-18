0b845315e8f329a44a0abdb95f677e11ff6b2bbf
srini047 <srinikethcr7@gmail.com>
2023-07-18 22:44:46 +0530

    fix: chat output

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
     # with st.chat_message("assistant"):
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = prompt
+            response = chat_with_pokemon_data(prompt)
             st.write(response)
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
