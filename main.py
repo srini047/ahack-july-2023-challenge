249d20a2d77486a90232f7a253c97f528f6609cb
srini047 <srinikethcr7@gmail.com>
2023-07-20 20:05:27 +0530

    fix: ui render issue

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ chat_history = []
 
 # App title
 st.set_page_config(page_title="🏙️💬 Data Story Telling")
-st.title("Data Visualisation")
+st.title("Data Visualisation & Intepretation")
 st.caption("Talk your way through data")
 
 INITIAL_MESSAGE = [
@@ -68,6 +68,29 @@ if st.session_state.messages[-1]["role"] != "assistant":
 ################################################################
 # Here it has to be updated with intro and about the dataset
 
+st.markdown(
+    """
+Welcome to the world of Pokemon, where adventure, strategy, and special creatures await! This dataset delves into the fascinating world of Pokemon games, not just maps and Pokemon Go. Here you'll uncover the mysteries of 721 unique Pokémon, each with their own traits and abilities.
+Our dataset comprehensively illustrates the traits that shape the strengths and abilities of these remarkable creatures. Get the core information right from the start.
+The dataset contains various traits of each Pokemon such as: B. Its her ID number, its name, and its primary and secondary his types. These types determine the Pokémon's vulnerability and resistance to various types of attacks, adding a strategic element to battles.
+Beyond types, we look at the basic stats that affect a Pokémon's performance in battle. These statistics include:
+HP (Hit Points):
+HP represents a Pokémon's health and determines how much damage it can take before being stunned. A higher HP value means a higher ability to withstand attacks.
+attack:
+Basic modifiers for normal attacks such as scratch and punch. Pokémon with higher attack stats have higher health in battle.
+defense:
+This stat measures a Pokémon's resistance to basic attacks, reducing the damage it takes. The higher the defense stat, the more durable the Pokémon will be.
+Special attack (SP attack):
+SP Atk indicates a Pokémon's ability to use unique and powerful abilities as base modifiers for special attacks such as Fire Blast and Bubble Jet.
+Special Defense (SP Def):
+SP Def reflects a Pokémon's resistance to special attacks and protects it from the full effects of such moves. The higher the SP Defense, the more the Pokémon's resistance to special abilities.
+speed:
+An important trait that determines a Pokémon's speed and attack order in battle. The higher the speed stat, the more advantage you have when attacking first.
+These stats culminate in overall attributes, which are a general indicator of a Pokémon's overall power. Summarizing the stats above, the overall traits serve as a helpful guide when assessing the relative strength of each Pokémon.
+This dataset serves as a great resource for statistics lessons, allowing you to introduce the concepts of data analysis and visualization into the fascinating world of Pokémon. Additionally, for specific Pokémon types, we can also explore the intersection of this dataset and the exciting realm of machine learning. Prepare for an immersive journey as you analyze, visualize, and interpret the data behind these extraordinary creatures. Together we will uncover hidden stories, uncover the mysteries of Pokemon battles, develop a better understanding of the intricacies of Pokemon battles, and grow you into an accomplished Pokemon Trainer.
+Grab your Poké Ball, choose your favorite Pokémon, and immerse yourself in adventure! The world of Pokémon awaits your arrival.
+"""
+)
 
 ################################################################
 # Plots
@@ -75,25 +98,94 @@ with st.container():
     # 1. Radar plot
     st.write("Radar plot")
     st.plotly_chart(radar_plot(df, "Charmander"))
+    st.markdown(
+        """
+- As the radar plot unfolds, we see a web of lines connecting various vertices, each representing a different attribute of the Pokemon`s stats.
+- By analyzing the plot, we can gain insights into the strengths and weaknesses of the Pokemon.
+- For instance, if the line for HP extends to a considerable length, it signifies that the Pokemon has a high health pool, allowing it to withstand significant damage in battles.
+- Similarly, a long line for Attack indicates the Pokemon possesses formidable offensive capabilities, delivering powerful blows to opponents.
+- A lengthy line for Defense suggests that the Pokemon is highly resilient, able to withstand attacks and minimize damage taken.
+- Furthermore, if the line for Special Attack or Special Defense extends far, it reveals the Pokemon's prowess in utilizing special attacks or defending against them, respectively.
+- A long line for Speed signifies exceptional agility, enabling the Pokemon to act swiftly and potentially strike first in battles.
+- By observing the shape and length of the lines connecting the vertices, trainers can quickly assess the overall profile of the Pokemon's stats.
+- They can identify areas of specialization and determine the Pokemon's role in battles, whether it excels in offense, defense, speed, or a combination of attributes.
+- The radar plot serves as a valuable tool for trainers, allowing them to make informed decisions when assembling their teams and devising battle strategies.
+- As trainers delve deeper into their Pokemon's radar plots, they gain a deeper understanding of the creature's unique abilities."""
+    )
 
     # 2. Contour plot (Not supported yet by Streamlit)
     st.write("Contour plot")
     st.plotly_chart(contour_plot(df))
+    st.markdown(
+        """
+- Contour maps made with HP, Attack, Defense, Attack, Defense, and Speed ​​attributes provide an interesting insight into the Pokémon world. This graph focuses on his first 10 data points in the dataset.
+- As the story progresses, interesting landscapes appear filled with outlines representing the various attribute values ​​of his first ten Pokémon. A light-to-dark color scale adds depth and dimension to your 
+charts, and helps you visualize the magnitude of attribute values.
+- Each contour line represents a specific attribute value, creating a kind of topographic map within the chart. By following contour lines, you can identify areas where certain attribute values ​​are more common 
+in the selected Pokémon.
+- The contour plot shows interesting patterns and trends. You may notice areas with relatively high HP numbers. This indicates that the Pokémon has high health and stamina.
+- In other regions, the outline may represent a concentration of high attack stats and represent Pokemon with great offensive ability, and likewise regions with increased defensive stats may highlight Pokemon with exceptional resilience and defensive abilities.
+- Observing the contours and their placement reveals relationships between various attributes. For example, closely spaced contour lines may indicate a positive correlation between HP and defensive attributes, suggesting that Pokemon with higher HP tend to have better defensive abilities.
+- This newfound knowledge allows trainers to make informed decisions when selecting teammates, considering the combination of attributes that fits their preferred combat strategy. 
+- You can identify Pokémon with balanced traits, or Pokémon with special strengths in certain areas.
+"""
+    )
 
     # 3. Heatmap plot
     st.write("Heatmap plot")
     st.plotly_chart(heatmap_plot(df))
+    # st.markdown("""""")
 
     # 4. Dist plot
     st.write("Distplot")
     st.plotly_chart(
         dist_plot(df, "HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed")
     )
+    st.markdown(
+        """
+- The Attack/Defense chart of the Pokemon dataset provides valuable insight into the characteristics of Pokemon teams. By examining the distribution of attack and defense statistics, we can draw some important conclusions.
+- First, the distinct spikes in the distplot indicate the existence of different clusters within the team. This suggests that Pokémon teams are made up of subgroups with different offensive and defensive attributes. This information allows you to identify different strengths and skills of your team members. Within the distribution, we observe moderate accumulation of attack and defense values. These Pokémon have a good balance of attack and defense, making them versatile and adaptable in battle. It is a reliable partner who plays an active part in various scenes.
+- In addition, we also found clusters with high offensive and defensive power. The team's powerful Pokémon boast outstanding offensive and defensive power. Their ability to withstand powerful attacks while delivering devastating strikes makes them the mainstay of the team in combat.
+- Additionally, outliers with unusually high attack or defense values ​​stand out in the distribution plot. These rare gems have extraordinary offensive or defensive power. They single-handedly have the potential to turn the tide of battle and are invaluable to the team.
+- Analyzing the distribution can also help you strategize your team. A distribution focused on high attack stats indicates an aggressive playstyle focused on overpowering opponents. Conversely, a distribution that tends to have higher defensive stats suggests a more defensive approach that emphasizes stamina and survival.
+- Overall, the attack/defense chart provides insight into team composition, balance, and potential strategy. This allows you to assess the strengths and weaknesses of individual Pokémon and make informed decisions about team composition, combat tactics, and training priorities.
+- This knowledge allows Trainers to optimize their Pokémon teams, capitalizing on their strengths and addressing their weaknesses. By leveraging the valuable information provided by Attack/Defense charts, budding Pokémon champions can pave their way to victory and become true champions in the immersive world of Pokémon. 
+"""
+    )
 
     # 5. Scatter plot - 3D
     st.write("Scatterplot 3D")
     st.plotly_chart(scatter_plot_3d(df, "Average Stats", "Atk-Def Ratio", "Speed"))
+    st.markdown(
+        """
+- In the enchanting realm of Pokemon, a magical 3D scatter plot materializes, unveiling the secrets of Attack, Defense, and Speed attributes.
+- Clusters of data points form constellations, signifying Pokemon with similar traits. Outliers shine brightly, embodying specialized strengths in specific attributes.
+- Trainers unlock strategic insights, crafting well-balanced teams  and harnessing the power of specialized Pokemon.
+- The plot becomes their guiding compass, shaping battle tactics and team composition.
+- It illuminates the intricate dance between offense, defense, and swiftness.
+- With every adventure, trainers embrace the plot's wisdom, honing their skills to become legendary masters of Pokemon.
+- As they embark on thrilling journeys, the 3D scatter plot serves as  a visual narrative, empowering trainers to navigate the complexities of attribute relationships. 
+- It fuels their passion, igniting the spark of imagination, and propelling them towards victory. 
+- With each strategic decision, trainers carve their path, weaving a tale of balance, specialization, and triumph.
+- The 3D scatter plot becomes a cherished companion, a magical tool that brings the world of Pokemon to life.
+- Through its vibrant colors and celestial clusters, trainers discover the boundless possibilities that lie ahead.
+In the realm of Pokemon, this data visualization becomes a beacon of knowledge, inspiring trainers to embark on extraordinary adventures and etch their names in the annals of Pokemon history.
+"""
+    )
 
     # 6. Top 10 plot (Bar graph)
     st.write("Top 10 plot")
     st.plotly_chart(top_ten_plot(df))
+    st.markdown(
+        """
+- The storyline features the top ten Pokémon characters based on their ‘‘overall‘‘ attributes, which serve as a general measure of a Pokémon`s overall power.
+- Examining this graph, we can draw the following conclusions:As the chart unfolds in front of you, you'll see an impressive collection of Pokémon that have achieved impressive total scores.
+- At the top of the table are the strongest Pokémon, whose high bar indicates their extraordinary strength.
+- If you look at the rest of the graph, you'll notice that the total score is gradually declining.
+- They still have the amazing ability to hold their own against their enemies and perform powerful attacks.
+- Each bar in the graph represents a Pokémon, and differences in length indicate differences in total points.
+- Trainers should consider recruiting these powerful creatures into their teams as they prepare to face difficult battles and terrifying foes.
+- This story evokes the amazing diversity and power of the Pokémon world.
+- If a Trainer aspires to become a Pokémon Master, he must carefully analyze the traits and abilities of his teammates.
+- The information presented in this visualization serves as a guide, showing you the strongest Pokémon that can lead you to victory in your quest to become a true champion."""
+    )
